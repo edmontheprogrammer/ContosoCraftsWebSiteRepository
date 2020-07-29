72c4942c12aee0b69796bdc2e74acfbed7e23730
edmontheprogrammer <41594880+edmontheprogrammer@users.noreply.github.com>
2020-07-29 11:13:27 +0800

    Contoso Crafts Website
    Contoso Crafts Website

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsService productsService)
+        {
+            this.ProductsService = productsService; 
+        }
+
+        public JsonFileProductsService ProductsService
+        {
+            get;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return (IEnumerable<Product>)ProductsService.GetProducts(); 
+        }
+    }
+}
